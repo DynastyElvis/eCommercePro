c0648fa4b0eb7aff0b166aa22372125fab574ce5
kipkemoilvs <kipkemoilvs@gmail.com>
2023-04-17 19:31:56 +0300

    ft:database configurations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique(0);
+            $table->string('usertype')->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
