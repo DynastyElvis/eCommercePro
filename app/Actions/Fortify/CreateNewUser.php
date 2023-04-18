a56f141a0d55dac1251d05561527819f16c485b4
kipkemoilvs <kipkemoilvs@gmail.com>
2023-04-18 10:59:15 +0300

    bg: vite bug fixes

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -29,6 +29,8 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'phone' => $input['phone'],
+            'address' => $input['address'],//check db table, they come from register blade
             'password' => Hash::make($input['password']),
         ]);
     }
