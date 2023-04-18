276a11f9ac0035d4c7be3b69e44bc2ebbbd7bfaa
kipkemoilvs <kipkemoilvs@gmail.com>
2023-04-18 21:45:26 +0300

    st: dismiss alert button feature

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -24,6 +24,7 @@
         <div class="content-wrapper">
             @if(session()->has('message'))
             <div class="alert alert-success">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
                 {{session()->get('message')}}
             </div>
             @endif
