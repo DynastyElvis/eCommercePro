3dd31fd53396554774cddaf7a30bb5c162864f91
kipkemoilvs <kipkemoilvs@gmail.com>
2023-05-04 10:26:43 +0300

    ft:redirect and routes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -57,8 +57,13 @@ class AdminController extends Controller
         $product=product::all();
         return view('admin.show_product',compact('product'));
     }
+    public function delete_product($id)
+    {
+        $product=product::find($id);
+        $product->delete();
+        return redirect()->back();
+    }
 
-    
 }
 
 
