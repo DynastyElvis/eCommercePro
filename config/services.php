dedf768f9815232755859af7c69ed8bc91aa770d
kipkemoilvs <kipkemoilvs@gmail.com>
2023-05-08 12:57:28 +0300

    ft:google cloud apis and keys

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'client_id' =>'5273682633-1alerpieslumlros89vqlctai8kjugjb.apps.googleusercontent.com',
+        'client_secret' =>'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',
+        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+    ],
 
 ];
